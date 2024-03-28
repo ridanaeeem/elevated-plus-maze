39fe8fffc00623926fd0a1e0d595547113fcb36b
ridanaeeem <ridan@bu.edu>
2024-03-28 14:42:05 -0400

    two sensors

--- epm.ino/epm.ino.ino
+++ epm.ino/epm.ino.ino
@@ -1,16 +1,30 @@
 // recording devices
 int distSensor = A0; 
-// recording values
-float sensorReading;
+int distSensor2 = A1;
+
+int max_rang = 520;
+double adc_solution = 1023.0;
+
+float distReading;
+float distReading2;
+float dist_t;
+float dist_t2;
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  sensorReading = analogRead(distSensor);
-  Serial.print(sensorReading, 0);
+  // read the value from the sensor:
+  distReading = analogRead(distSensor);
+  distReading2 = analogRead(distSensor2);
+  dist_t = distReading * max_rang / adc_solution;  //
+  dist_t2 = distReading2 * max_rang / adc_solution;  //
+  Serial.print(dist_t, 0);
+  Serial.print("cm ");
+  Serial.print(dist_t2, 0);
   Serial.println("cm");
 
   delay(100);
 }
+
