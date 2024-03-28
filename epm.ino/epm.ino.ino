0179bc49029b026296f9f88aec5b9b72d97517cb
ridanaeeem <ridan@bu.edu>
2024-03-28 14:47:15 -0400

    four sensors

--- epm.ino/epm.ino.ino
+++ epm.ino/epm.ino.ino
@@ -1,14 +1,21 @@
 // recording devices
-int distSensor = A0; 
+int distSensor1 = A0; 
 int distSensor2 = A1;
+int distSensor3 = A2;
+int distSensor4 = A3;
 
-int max_rang = 520;
+int max_range = 520;
 double adc_solution = 1023.0;
 
-float distReading;
+float distReading1;
 float distReading2;
-float dist_t;
-float dist_t2;
+float distReading3;
+float distReading4;
+
+float distCm1;
+float distCm2;
+float distCm3;
+float distCm4;
 
 void setup() {
   Serial.begin(9600);
@@ -16,14 +23,26 @@ void setup() {
 
 void loop() {
   // read the value from the sensor:
-  distReading = analogRead(distSensor);
+  distReading1 = analogRead(distSensor1);
   distReading2 = analogRead(distSensor2);
-  dist_t = distReading * max_rang / adc_solution;  //
-  dist_t2 = distReading2 * max_rang / adc_solution;  //
-  Serial.print(dist_t, 0);
+  // distReading3 = analogRead(distSensor3);
+  // distReading4 = analogRead(distSensor4);
+
+  // account for range vs 10 bit accuracy
+  distCm1 = distReading1 * max_range / adc_solution;  
+  distCm2 = distReading2 * max_range / adc_solution;  
+  // distCm1 = distReading3 * max_range / adc_solution;  
+  // distCm2 = distReading4 * max_range / adc_solution;  
+
+  // print results
+  Serial.print(distCm1, 0);
   Serial.print("cm ");
-  Serial.print(dist_t2, 0);
+  Serial.print(distCm2, 0);
   Serial.println("cm");
+  // Serial.print(distCm3, 0);
+  // Serial.print("cm ");
+  // Serial.print(distCm4, 0);
+  // Serial.println("cm");
 
   delay(100);
 }
