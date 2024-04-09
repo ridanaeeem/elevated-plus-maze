d78c898c9a68c012aab2ee73c227ea953f3b3493
ridanaeeem <ridan@bu.edu>
2024-04-09 15:27:11 -0400

    trying to get i2c multiple addressing to work

--- epm.ino/epm.ino.ino
+++ epm.ino/epm.ino.ino
@@ -2,11 +2,20 @@
 #include "Wire.h"
 #include "DFRobot_VL53L0X.h"
 
+// things to record
+// time spent in open arms
+// latency to middle
+// time freezing before crossing into middle
+// time freezing (first 2 min)
+// freezing before crossing/latency to middle = % time frozen 
+
 // recording devices
-int distSensor1 = A0;
-int distSensor2 = A1;
+// int distSensor1 = A0;
+// int distSensor2 = A1;
 // int distSensor3 = A2;
 // int distSensor4 = A3;
+int xShutTop = 8;
+int xShutRight = 9;
 
 // raw readings
 float distReading1;
@@ -33,6 +42,7 @@ double adc_solution = 1023.0;
 
 //sensor 3
 DFRobot_VL53L0X sensor;
+DFRobot_VL53L0X sensorRight;
 
 float leftInit;
 float leftSum = 0;
@@ -46,88 +56,122 @@ float topI = 0;
 
 void setup() {
   Serial.begin(9600);
+
+  pinMode(xShutTop, OUTPUT);
+  digitalWrite(xShutTop, LOW);
+  pinMode(xShutRight, OUTPUT);
+  digitalWrite(xShutRight, LOW);
+  delay(10);
+  digitalWrite(xShutTop, HIGH);
+  pinMode(xShutTop, INPUT);
+
+  Wire.begin(); // Initialize I2C communication
+
+  // Set I2C sub-device address for sensor
+  sensor.begin();
+  // sensorRight.begin(0x51);
+  sensor.setMode(sensor.eContinuous, sensor.eHigh);
+  sensor.start();
+
+  digitalWrite(xShutRight, HIGH);
+  pinMode(xShutRight, INPUT);
+
+  sensorRight.begin(0x30);
+  // sensorRight.begin(0x51);
+  sensorRight.setMode(sensor.eContinuous, sensor.eHigh);
+  sensorRight.start();
+
+  // // Set I2C sub-device address for sensorRight
+  // sensorRight.begin(0x51);
+  // sensorRight.setMode(sensor.eContinuous, sensor.eHigh);
+  // sensorRight.start();
+
+  // Turn off all ports for both devices
+  // Wire.beginTransmission(0x50); // device 1
+  // Wire.write(0x00); // all ports off
+  // Wire.endTransmission();
+
+  // Wire.beginTransmission(0x51); // device 2
+  // Wire.write(0x00); // all ports off
+  // Wire.endTransmission();
+  // sensor.setAddress(0x2A + 1);
+  // sensors.startContinuous(50);
   // for sensor3
   //join i2c bus (address optional for master)
-  Wire.begin();
-  //Set I2C sub-device address
-  sensor.begin(0x50);
-  //Set to Back-to-back mode and high precision mode
-  sensor.setMode(sensor.eContinuous,sensor.eHigh);
-  //Laser rangefinder begins to work
-  sensor.start();
+  // Wire.begin();
+  // //Set I2C sub-device address
+  // sensor.begin(0x50);
+  // //Set to Back-to-back mode and high precision mode
+  // sensor.setMode(sensor.eContinuous,sensor.eHigh);
+  // //Laser rangefinder begins to work
+  // sensor.start();
+
+
+  // //Set I2C sub-device address
+  // sensorRight.begin(0x51);
+  // //Set to Back-to-back mode and high precision mode
+  // sensorRight.setMode(sensor.eContinuous,sensor.eHigh);
+  // //Laser rangefinder begins to work
+  // sensorRight.start();
+
+  // Wire.begin();
+  // Wire.beginTransmission(0x50); // device 1
+  // Wire.write(0x00); // all ports off
+  // Wire.endTransmission();
+  // Wire.begin();
+  // Wire.beginTransmission(0x51); // device 2
+  // Wire.write(0x00); // all ports off
+  // Wire.endTransmission();
 }
 
 void loop() {
   // read the value from the sensor:
-  distReading1 = analogRead(distSensor1);
-  distReading2 = analogRead(distSensor2);
+  distReading1 = sensorRight.getDistance();
+  // distReading2 = analogRead(distSensor2);
   distReading3 = sensor.getDistance();
   // distReading4 = analogRead(distSensor4);
 
   // account for range vs 10 bit accuracy
-  distCm1 = distReading1 * max_range / adc_solution;
-  distCm2 = distReading2 * max_range / adc_solution;
+  distCm1 = (distReading1 - 25) / 10;
+  // distCm2 = distReading2 * max_range / adc_solution;
   distCm3 = (distReading3 - 25) / 10;
   // distCm2 = distReading4 * max_range / adc_solution;
 
-  // add to array for averaging
-  // float avg1 = 0;
-  // float avg2 = 0;
-  // if (avgIndex < nAvg) {
-  //   avgIndex++;
-  //   // average the array
-  //   float tot1 = 0;
-  //   float tot2 = 0;
-  //   for (int i = 0; i < nAvg; i++) {
-  //     tot1 += average1[i];
-  //     tot2 += average1[i];
-  //   }
-  //   avg1 = tot1 / nAvg;
-  //   avg2 = tot2 / nAvg;
-  // } else avgIndex = 0;
-  // average1[avgIndex] = distCm1;
-  // average2[avgIndex] = distCm2;
-  // average3[avgIndex] = distCm3;
-  // average4[avgIndex] = distCm4;
-
-  // print results
-  // Serial.print(avg1, 0);
-  // Serial.print("cm ");
-  // Serial.print(avg2, 0);
-  // Serial.println("cm");
-
   // calibration
-  if (millis() < 5000){
-    rightSum += distCm1;
-    rightI ++;
-    leftSum += distCm2;
-    leftI ++;
-    topSum += distCm3;
-    topI ++;
-    Serial.println("waiting");
-  } else if (millis() > 5000 && millis() < 6000) {
-    rightInit = rightSum/rightI;
-    leftInit = leftSum/leftI;
-    topInit = topSum/topI;
-    Serial.println(rightInit);
-    Serial.println(leftInit);
-    Serial.println(topInit);
-  } else {
-    Serial.print("right ");
-    Serial.print(distCm1, 0);
-    Serial.print("cm ");
-    Serial.print("left ");
-    Serial.print(distCm2, 0);
-    Serial.print("cm ");
-    Serial.print("top ");
-    Serial.print(distCm3, 0);
-    Serial.println("cm");
-  }
+  // if (millis() < 5000){
+  //   rightSum += distCm1;
+  //   rightI ++;
+  //   leftSum += distCm2;
+  //   leftI ++;
+  //   topSum += distCm3;
+  //   topI ++;
+  //   Serial.println("waiting");
+  // } else if (millis() > 5000 && millis() < 6000) {
+  //   rightInit = rightSum/rightI;
+  //   leftInit = leftSum/leftI;
+  //   topInit = topSum/topI;
+  //   Serial.print("right ");
+  //   Serial.print(rightInit);
+  //   Serial.print("cm left ");
+  //   Serial.print(leftInit);
+  //   Serial.print("cm top ");
+  //   Serial.print(topInit);
+  //   Serial.println("cm");
+  // } else {
+  //   Serial.print("right ");
+  //   Serial.print(distCm1, 0);
+  //   Serial.print("cm left ");
+  //   Serial.print(distCm2, 0);
+  //   Serial.print("cm top ");
+  //   Serial.print(distCm3, 0);
+  //   Serial.println("cm");
+  // }
+  Serial.print("right ");
+  Serial.print(distCm1, 0);
+  // Serial.print("cm left ");
+  // Serial.print(distCm2, 0);
+  Serial.print(" cm top ");
+  Serial.print(distCm3, 0);
+  Serial.println("cm");
   delay(100);
 }
-
-#include "Arduino.h"
-#include "Wire.h"
-#include "DFRobot_VL53L0X.h"
-
-
